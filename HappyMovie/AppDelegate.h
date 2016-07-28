1fbbe03cffd6192f77e34badcc1f22ad5a5953f6
赵传保 <zhaochuanbao@2clooiMac.local>
2016-07-28 20:00:46 +0800

    第一版

---
+++ HappyMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyMovie
+//
+
+//  Copyright © 2016年 刘培培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform:'ios','8.0'
+pod 'UMengSocial', '~> 4.4'
+pod 'AVOSCloud', '~> 3.1.7'
+pod 'AFNetworking', '~> 3.0.4'
